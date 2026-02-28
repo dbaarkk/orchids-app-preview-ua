5fd0e0db3945b83e1323303c8c0aae2e291a769c
QuickBoost <legend159980@gmail.com>
2026-02-28 16:41:20 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,8 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.urbanauto.app',
-  appName: 'Urban Auto',
-  webDir: 'out',
-  plugins: {
-    PushNotifications: {
-      presentationOptions: ["badge", "sound", "alert"]
-    }
+const config = {
+  appId: "com.urbanauto.app",
+  appName: "Urban Auto",
+  server: {
+    url: "https://app.theurbanauto.com",
+    cleartext: false
   }
 };
-
-export default config;
